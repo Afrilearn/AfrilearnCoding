1dfa76eb24a9d24170e55b7cc3bed693f0ee40ce
Michael <micheaol80@gmail.com>
2022-07-02 17:49:06 +0100

    Add style to the sponsor section

--- src/components/Unicef.js
+++ src/components/Unicef.js
@@ -1,61 +1,105 @@
 import React from 'react';
 import './unicef.css';
+import './imageholder.css'
+import Slider from "react-slick";
 
 import unicef from '../images/unicef-logo.png';
-import global from '../images/global-logo.png';
-import business from '../images/business.jpg';
-import guardian from '../images/guardian-logo.png';
-import techcabal from '../images/tech-cabal-logo.png';
+import Technext from '../images/technext.png';
+import TheNation from '../images/thenation.jpg';
+import guardian from '../images/guadian.png';
+import techcabal from '../images/TC.png';
 import venture from '../images/ventureburn-logo.png';
-// import techcabal from '../images/tech-cabal-logo.png';
+import USChambers from '../images/us-chamber-logo.png';
+import gc from '../images/gc-logo-no-space.png';
+import BusinessDay from '../images/business.jpg';
+import techbuild from '../images/techbuild.png';
 
 const Unicef = () => {
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear"
+  };
+
+
   return (
-    <div className='endorsed-main-wrapper'>
-        <div className='main-title'>
-            <h1>As Endorsed by</h1>
-        </div>
-        <div className='main-endorsed-wrapper'>
-            <ul>
-              <li>
-                <img
-                  src={unicef}
-                />
-                <span>UNICEF</span>
-              </li>
-              <li>
-                <img
-                    src={business}
-                  />
-                <span>Global Citizen</span>
-              </li>
-              <li>
-                <img
-                    src={guardian}
-                  />
-                <span>BusinessDay</span>
-              </li>
-              <li>
-              <img
-                  src={techcabal}
-                />
-                <span>TechCabal</span>
-              </li>
-              <li>
-                <img
-                    src={techcabal}
-                  />
-                <span>The Guardian</span>
-              </li>
-              <li>
-                <img
-                    src={venture}
-                  />
-                <span>VentureBurn</span>
-              </li>
-            </ul>
-        </div>
-    </div>
+    <>
+      <div className="title-holder-wrapper">
+           <h1>Endorsed by:</h1>
+      </div>
+ 
+    <div className='main-image-wrapper-endorsed'>
+      
+    <Slider {...settings}  >
+         <div  className='first-image-holder-endorsed'>
+            <img
+                 src={unicef}
+                 alt="unicef"
+            />
+         </div>
+         <div className='second-image-holder-endorsed'>
+         <img
+             src={Technext}
+             alt="Technext"
+            />
+         </div>
+         <div className='third-image-holder-endorsed'>
+         <img
+                 src={TheNation}
+                 alt="TheNation"
+            />
+         </div>
+         <div className='fourth-image-holder-endorsed'>
+         <img
+             src={guardian}
+             alt="guardian"
+            />
+         </div>
+         <div className='fourth-image-holder-endorsed'>
+         <img
+             src={techcabal}
+             alt="techcabal"
+            />
+         </div>
+         <div className='fourth-image-holder-endorsed'>
+         <img
+             src={venture}
+             alt="venture"
+            />
+         </div>
+         <div className='fourth-image-holder-endorsed'>
+         <img
+             src={USChambers}
+             alt="USChambers"
+            />
+         </div>
+         <div className=' fourth-image-holder-endorsed'>
+         <img
+             src={gc}
+             alt="gc"
+            />
+         </div>
+         <div className='fourth-image-holder-endorsed'>
+         <img
+             src={BusinessDay}
+             alt="BusinessDay"
+            />
+         </div>
+         <div className='fourth-image-holder-endorsed'>
+         <img
+             src={techbuild}
+             alt="techbuild"
+            />
+         </div>
+         </Slider>
+         </div>
+         </>
   )
 }
 
---
+++ src/components/unicefData.js
@@ -0,0 +1,7 @@
+export const unicefData = [
+    {
+        id: 1,
+        imageUrl: '../images/unicef-logo.png',
+        endorName: 'unicef'
+    },
+]
